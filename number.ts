6cd3d943afd4efd98a7c7cf8b4c9365d7f784ff9
pchalupa <chalupa.petr93@gmail.com>
2025-08-19 08:24:30 +0200

    fix format

--- number.ts
+++ number.ts
@@ -2,6 +2,9 @@ export function isNumber(value: unknown): value is string {
 	return typeof value === "number";
 }
 
-export function assertNumber(value: unknown, message = `Expected a number, received ${typeof value}`): asserts value is number {
+export function assertNumber(
+	value: unknown,
+	message = `Expected a number, received ${typeof value}`,
+): asserts value is number {
 	if (typeof value !== "number") throw new TypeError(message);
 }
--- string.ts
+++ string.ts
@@ -2,6 +2,9 @@ export function isString(value: unknown): value is string {
 	return typeof value === "string";
 }
 
-export function assertString(value: unknown, message = `Expected a string, received ${typeof value}`): asserts value is string {
+export function assertString(
+	value: unknown,
+	message = `Expected a string, received ${typeof value}`,
+): asserts value is string {
 	if (typeof value !== "string") throw new TypeError(message);
 }
